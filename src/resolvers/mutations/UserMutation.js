26fccd87d2ce1eb4360cdc9c5da82edcbfff1e26
Haider Malik <haidermalik504@gmail.com>
2018-07-20 18:20:19 +0500

    implement user login

--- src/resolvers/mutations/UserMutation.js
+++ src/resolvers/mutations/UserMutation.js
@@ -15,6 +15,27 @@ async function signup(parent, { name, email, password }, ctx, info) {
         user
     }
 }
+async function login(parent, { email, password }, ctx, info) {
+    const user = await ctx.db.query.user({
+        where: {
+            email
+        }
+    }, `{id password}`);
+    if (!user) {
+        throw new Error('No such a user');
+    }
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+        throw new Error('Invalid password');
+    }
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    return {
+        token,
+        user
+    }
+
+}
 module.exports = {
-    signup
+    signup,
+    login
 }
