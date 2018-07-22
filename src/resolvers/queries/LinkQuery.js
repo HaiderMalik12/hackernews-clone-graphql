fc8877d307285d4a5b13c2ab97dfec3be0beab30
Haider Malik <haidermalik504@gmail.com>
2018-07-22 09:24:21 +0500

    implement filtering

--- src/resolvers/queries/LinkQuery.js
+++ src/resolvers/queries/LinkQuery.js
@@ -1,5 +1,13 @@
 function linksFeed(parent, args, ctx, info) {
-    return ctx.db.query.links({}, info)
+    const where = args.filter
+        ? {
+            OR: [
+                { url_contains: args.filter },
+                { description_contains: args.filter }
+            ]
+        }
+        : {};
+    return ctx.db.query.links({ where }, info)
 }
 function singleLink(parent, { id }, ctx, info) {
     return ctx.db.query.link({ where: { id } }, info)
